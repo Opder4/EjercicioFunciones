36a42bb8754175932dbcf4d689f56b582fdf5bb9
Emanuel Ramírez <er6348100@gmail.com>
2026-03-12 16:27:15 -0600

    Fin Ej 13

--- 13/Program.cs
+++ 13/Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿using System.Net.Http.Headers;
+
+double TotalCompras(int a)
+{
+    double suma = 0;
+    for (int i = 1; i <= a; i++)
+    {
+        Console.WriteLine($"Ingrese la {i}° compra") ;
+        Console.WriteLine();
+        double b=double.Parse( Console.ReadLine() );
+        Console.WriteLine();
+        if (b>=0)
+        { 
+        suma += b;
+        }
+        else
+        {
+            Console.WriteLine("Precio de compra no válido");
+            Console.WriteLine();
+        }
+    }
+        return suma;
+}
+Console.WriteLine("Ingrese la cantidad de compras que realizó");
+Console.WriteLine();
+int cantidadcompras=int.Parse( Console.ReadLine() );
+Console.WriteLine();
+if  ( cantidadcompras>0 )
+{
+    Console.WriteLine($"El precio total final es Q{TotalCompras(cantidadcompras)}");
+}
+else
+{
+    Console.WriteLine("Cantidad de compras no válida");
+}
