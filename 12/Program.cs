5f849001140bf3d8846c9dc4de50d7769863a55d
Emanuel Ramírez <er6348100@gmail.com>
2026-03-12 16:15:46 -0600

    Fin Ej 12

--- 12/Program.cs
+++ 12/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿void edad(int a)
+{
+    Console.WriteLine($"Su edad es {a}"); ;
+}
+Console.WriteLine("Ingrese su edad");
+Console.WriteLine();
+int age=int.Parse(Console.ReadLine());
+Console.WriteLine();
+if (age>=0)
+{ 
+edad(age);
+}
+else
+{
+    Console.WriteLine("Edad no válida");
+}
