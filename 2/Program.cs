dfd96ccd875db4063b5b9f817f2297a3f94f16f3
Emanuel Ramírez <er6348100@gmail.com>
2026-03-12 15:25:07 -0600

    Fin Ej 2

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿double cuadrado(double a)
+{
+    return a * a;
+}
+Console.WriteLine("Ingrese un número");
+Console.WriteLine();
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"El cuadrado es {cuadrado(a)}");
