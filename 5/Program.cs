c96fd5fb514d1ceeac3701697ef70c31fc1a0435
Emanuel Ramírez <er6348100@gmail.com>
2026-03-12 15:38:29 -0600

    Fin Ej 5

--- 5/Program.cs
+++ 5/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System.Threading.Channels;
+
+double arearec(double a, double b)
+{
+    return a * b;
+}
+Console.WriteLine("Ingrese la altura del rectángulo");
+Console.WriteLine();
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Ingrese la base del rectángulo");
+Console.WriteLine();
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine();
+if  (a<0 || b<0)
+{
+    Console.WriteLine("Ingrese datos válidos");
+}
+else
+{
+    Console.WriteLine($"El área del rectángulo es {arearec(a,b)}");
+}
