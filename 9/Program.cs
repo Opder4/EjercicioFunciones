ac277db196ea1a1c9ca9734a8c6b8c676f3e4722
Emanuel Ramírez <er6348100@gmail.com>
2026-03-12 15:58:13 -0600

    Fin Ej 9

--- 9/Program.cs
+++ 9/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System.Xml;
+
+double salariofinal(double a, double b)
+{
+    return a * b;
+}
+Console.WriteLine("Ingrese su salario por hora");
+Console.WriteLine();
+double salario = double.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Ingrese las horas trabajadas");
+Console.WriteLine();
+double horastotales = double.Parse(Console.ReadLine());
+Console.WriteLine();
+if (horastotales >= 0 && salario>0)
+{
+    Console.WriteLine($"Su salario final es de Q{salariofinal(salario, horastotales)}");
+}
+else
+{
+    Console.WriteLine("Ingrese datos válidos");
+}
