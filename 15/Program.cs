ae409b64f9b0e4d03032829038c4435868c2ecdf
Emanuel Ramírez <er6348100@gmail.com>
2026-03-12 16:37:42 -0600

    Fin Ej 15

--- 15/Program.cs
+++ 15/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿double AreaTriangulo(double a, double b)
+{
+    return (a * b) / 2;
+}
+Console.WriteLine("Ingrese la altura del triángulo");
+Console.WriteLine();
+double altura = double.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Ingrese la base del triángulo");
+Console.WriteLine();
+double basse = double.Parse(Console.ReadLine());
+Console.WriteLine();
+if  (altura>0 && basse>0)
+{
+    Console.WriteLine($"El área del triángulo es de {AreaTriangulo(altura,basse)} unidades cuadradas");
+}
+else
+{
+    Console.WriteLine("Datos no válidos");
+}
