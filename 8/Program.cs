5b44bba44724c732bbc79fcbc57109b21f58b191
Emanuel Ramírez <er6348100@gmail.com>
2026-03-12 15:53:28 -0600

    Fin Ej 8

--- 8/Program.cs
+++ 8/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿double promedio(double suma)
+{
+    return (suma) / 3;
+}
+double suma = 0;
+for (int i=1; i<=3; i++)
+{
+    Console.WriteLine($"Ingrese el {i}° número");
+    Console.WriteLine();
+    double num = double.Parse(Console.ReadLine());
+    Console.WriteLine();
+    suma += num;
+}
+Console.WriteLine($"La el promedio de los tres fue {promedio(suma)}");
