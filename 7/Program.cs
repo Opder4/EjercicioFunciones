8d18f414f981cc405155e00c852ced1a3f4f4542
Emanuel Ramírez <er6348100@gmail.com>
2026-03-12 15:48:12 -0600

    Fin Ej 7

--- 7/Program.cs
+++ 7/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿double preciofinal(double precio, double desc)
+{
+    return precio - ((precio * desc) / 100);
+}
+Console.WriteLine("Ingrese el precio del producto");
+Console.WriteLine();
+double prec = double.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Ingrese el porcentaje (%) del descuento");
+Console.WriteLine();
+double des = double.Parse(Console.ReadLine());
+Console.WriteLine();
+if  (prec>=0 && des>=0)
+{
+    Console.WriteLine($"El precio final es {preciofinal(prec,des)}");
+}
+else
+{
+    Console.WriteLine("Los datos no son válidos");
+}
