9aad9fc3db0891266532631031e79fed5cd58aa3
Emanuel Ramírez <er6348100@gmail.com>
2026-03-12 16:40:33 -0600

    Fin Ej 16

--- 16/Program.cs
+++ 16/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿double ConversionMinutos(double m)
+{
+    return m / 60;
+}
+Console.WriteLine("Ingrese la cantidad de minutos");
+Console.WriteLine();
+double minutos=double.Parse(Console.ReadLine());
+Console.WriteLine();
+if  (minutos >= 0)
+{
+    Console.WriteLine($"Son {ConversionMinutos(minutos)} hora/s");
+}
+else
+{
+    Console.WriteLine("Minutos no válidos");
+}
