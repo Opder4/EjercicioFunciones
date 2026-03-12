6fa481e172fdd303cfff422737b27a0c7ec64087
Emanuel Ramírez <er6348100@gmail.com>
2026-03-12 16:33:09 -0600

    Fin Ej 14

--- 14/Program.cs
+++ 14/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿double VelocidadPromedio(double a, double b)
+{
+    return a / b;
+}
+Console.WriteLine("Ingrese la distancia recorridad por el emprealdo ");
+Console.WriteLine();
+double distancia = double.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Ingrese el tiempo hecho por el emprealdo ");
+Console.WriteLine();
+double tiempo = double.Parse(Console.ReadLine());
+Console.WriteLine();
+if (distancia>=0 && tiempo>0)
+{
+    Console.WriteLine($"La velocidad promedio del empleado es de {VelocidadPromedio(distancia, tiempo)} m/s");
+}
+else
+{
+    Console.WriteLine("Datos no válidos");
+}
