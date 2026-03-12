61601f2501318a3438ca1a0a1ce295acedefaae4
Emanuel Ramírez <er6348100@gmail.com>
2026-03-12 16:00:08 -0600

    Fin Ej 10

--- 10/Program.cs
+++ 10/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using System.Threading.Channels;
+
+double cubo(double a)
+{
+    return a * a * a ;
+}
+Console.WriteLine("Ingrese un número");
+Console.WriteLine();
+double numero = double.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"El cubo del número es {cubo(numero)}");
