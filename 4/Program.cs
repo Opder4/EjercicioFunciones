7bd126999f8d3cd76479fa5e5824aaa6a0944db7
Emanuel Ramírez <er6348100@gmail.com>
2026-03-12 15:32:16 -0600

    Fin Ej 4

--- 4/Program.cs
+++ 4/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿double areacirculo(double a)
+{
+    return a * Math.PI;
+}
+Console.WriteLine("Ingrese el Radio (r) del círculo");
+Console.WriteLine();
+double a=double.Parse(Console.ReadLine());
+Console.WriteLine();
+if (a<=0)
+{
+    Console.WriteLine("Ingrese un Radio (r) válido");
+}
+else
+{
+    Console.WriteLine($"El área del círculo es {areacirculo(a)}");
+}
