c4407eaa944f7eb96e26142dd7ed6b0f7931edf8
Emanuel Ramírez <er6348100@gmail.com>
2026-03-12 17:59:19 -0600

    Fin Ej 28

--- 28/Program.cs
+++ 28/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿void Promedio()
+{
+    double suma = 0;
+    for (int i=1; i<=3; i++)
+    {
+        Console.WriteLine($"Ingrese la {i}° nota");
+        Console.WriteLine();
+        double notas = double.Parse(Console.ReadLine());
+        Console.WriteLine();
+        if (notas>=0 && notas<=100)
+        {
+            suma += notas;
+        }
+        else
+        { 
+            Console.WriteLine("Nota no válida");
+        i = i - 1;
+        Console.WriteLine();
+        }
+    }
+    Console.WriteLine($"El promedio del estudiante fue {suma/3}");
+}
+Promedio();
