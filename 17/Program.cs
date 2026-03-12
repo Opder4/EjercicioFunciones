aac39c2491e9aca56d5c4bd36e9c2b73b5434718
Emanuel Ramírez <er6348100@gmail.com>
2026-03-12 16:47:20 -0600

    Fin Ej 17

--- 17/Program.cs
+++ 17/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿double PromedioGrupo(int a)
+{
+    double suma = 0;
+    for (int i=1; i<=a; i++)
+    {
+        Console.WriteLine($"Ingrese la nota del {i}° estudiante");
+        Console.WriteLine();
+        double notas = double.Parse(Console.ReadLine());
+        Console.WriteLine();
+        if (notas>=0 && notas<=100)
+        {
+            suma += notas;
+        }
+        else
+        {
+            Console.WriteLine("Nota no válida");
+            Console.WriteLine();
+        }
+    }
+    return suma/a;
+}
+Console.WriteLine("Ingrese la cantidad de estudiantes");
+Console.WriteLine();
+int cantidadestudiante=int.Parse(Console.ReadLine());
+Console.WriteLine();
+if (cantidadestudiante>0)
+{
+    Console.WriteLine($"El promedio del grupo fue de {PromedioGrupo(cantidadestudiante)}");
+}
+else
+{
+    Console.WriteLine("Dato no váldio");
+}
