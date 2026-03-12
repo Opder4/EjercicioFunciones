d12743002cac639732eca4f510039be7c4ba9b7e
Emanuel Ramírez <er6348100@gmail.com>
2026-03-12 15:27:51 -0600

    Fin Ej 3

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using System.Threading.Channels;
+
+double doble(double a)
+{
+    return 2*a;
+}
+Console.WriteLine("Ingrese un número");
+Console.WriteLine();
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"El doble es {doble(a)}");
