3b7cac6a64d8666096b52cebe5953b13060dc4db
Emanuel Ramírez <er6348100@gmail.com>
2026-03-12 16:06:36 -0600

    Fin Ej 11

--- 11/Program.cs
+++ 11/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿double ConversionKg(double a)
+{
+    return  a*2.205;
+}
+Console.WriteLine("Ingrese el número de Kilogramos (Kg)");
+Console.WriteLine();
+double kilo=double.Parse(Console.ReadLine());
+Console.WriteLine();
+if (kilo>=0)
+{ 
+Console.WriteLine($"La conversión Libras (lb) es {ConversionKg(kilo)} lb");
+}
+else
+{
+    Console.WriteLine("Dato inválido");
+}
