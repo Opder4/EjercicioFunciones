423599ba00ce788786b0597300eb15e10a8ccd00
Emanuel Ramírez <er6348100@gmail.com>
2026-03-12 17:48:42 -0600

    Fin Ej 26

--- 26/Program.cs
+++ 26/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿void BirthDay()
+{
+    Console.WriteLine("Ingrese su edad actual");
+    Console.WriteLine();
+    int edad=int.Parse(Console.ReadLine());
+    Console.WriteLine();
+    if (edad>=0)
+    {
+        Console.WriteLine($"Su año de nacimiento aproximado es {2026-edad}");
+    }
+    else
+    {
+        Console.WriteLine("Edad no válida");
+    }
+}
+BirthDay();
