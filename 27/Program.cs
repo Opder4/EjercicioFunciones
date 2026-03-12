51d9162151fdecae5e7198e3256b1742ba268fb9
Emanuel Ramírez <er6348100@gmail.com>
2026-03-12 17:52:53 -0600

    Fin Ej 27

--- 27/Program.cs
+++ 27/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿void PrecioFinal()
+{
+    Console.WriteLine("Ingrese el precio de un producto");
+    Console.WriteLine();
+    double precio=double.Parse(Console.ReadLine());
+    Console.WriteLine();
+    if (precio >= 0 )
+    {
+        Console.WriteLine($"El precio con 10% de descuento es {precio-(precio*0.10)}");
+    }
+    else
+    {
+        Console.WriteLine("Precio no válido");
+    }
+}
+PrecioFinal();
