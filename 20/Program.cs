044583ed1ac4c616f78e88d0bf8919d83e2fee6c
Emanuel Ramírez <er6348100@gmail.com>
2026-03-12 17:24:33 -0600

    Fin Ej 20

--- 20/Program.cs
+++ 20/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿    double SalarioFinal(double a, double b)
+    {
+        return a * b;
+    }
+    Console.WriteLine("Ingrese su salario diario");
+    Console.WriteLine();
+    double salario = double.Parse(Console.ReadLine());
+    Console.WriteLine();
+    if (salario > 0)
+    {
+        Console.WriteLine("Ingrese los días trabajados");
+        Console.WriteLine();
+        double diastotales = double.Parse(Console.ReadLine());
+        Console.WriteLine();
+        if (diastotales >= 0)
+        {
+            Console.WriteLine($"Su salario final es Q{SalarioFinal(salario, diastotales)}");
+        }
+        else
+        {
+            Console.WriteLine("Dato no válido");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Salario no válido");
+    }
