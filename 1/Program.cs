1e070f41beb6fccb13d1c041bd1003523f59456f
Emanuel Ramírez <er6348100@gmail.com>
2026-03-12 15:21:59 -0600

    Fin Ej 1

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine();
+﻿double suma(double a, double b)
+{
+    return a + b;
+}
+Console.WriteLine("Ingrese el primer número");
+Console.WriteLine();
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("ingrese el segundo número");
+Console.WriteLine();
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"La suma es {suma(a,b)}");
