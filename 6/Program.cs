51bf09a926526475ba1a6dff15951d4ab1101970
Emanuel Ramírez <er6348100@gmail.com>
2026-03-12 15:41:57 -0600

    Fin Ej 6

--- 6/Program.cs
+++ 6/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿double conversionFh(double a)
+{
+    return (a*(9/5))+35;
+}
+Console.WriteLine("Ingrese la cantidad de Celsius");
+Console.WriteLine();
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"La conversión a F° es {conversionFh(a)}");
