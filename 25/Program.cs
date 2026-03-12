9699bc2465a140d4ea2f0e45d3048e5c4473c144
Emanuel Ramírez <er6348100@gmail.com>
2026-03-12 17:45:04 -0600

    Fin Ej 25

--- 25/Program.cs
+++ 25/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿void AreaRectangulo()
+{
+    Console.WriteLine("Ingrese la altura del rectangulo");
+    Console.WriteLine();
+    double altura=double.Parse(Console.ReadLine());
+    Console.WriteLine();
+    Console.WriteLine("Ingrese la base del rectangulo");
+    Console.WriteLine();
+    double basee = double.Parse(Console.ReadLine());
+    Console.WriteLine();
+    if (altura>0 && basee>0)
+    {
+        Console.WriteLine($"El área del rectáguno es {basee*altura} unidades cuadradas");
+    }
+    else
+    {
+        Console.WriteLine("Datos no válidos");
+    }
+}
+AreaRectangulo();
