0b988f614696d9cc91c54fed2b389ad2c3a8a642
Emanuel Ramírez <er6348100@gmail.com>
2026-03-12 17:33:11 -0600

    Fin Ej 23

--- 23/Program.cs
+++ 23/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿void Suma()
+{
+    double suma = 0;
+    for (int i=1; i<=2; i++)
+    {
+        Console.WriteLine("Ingrese un número");
+        Console.WriteLine();
+        double numero=double.Parse(Console.ReadLine());
+        Console.WriteLine();
+        suma += numero;
+    }
+    Console.WriteLine($"La suma de los números fue {suma}");
+}
+Suma();
