33e8b1572fc12ed212c8b7c420755d62d5cd6b42
Emanuel Ramírez <er6348100@gmail.com>
2026-03-12 18:02:07 -0600

    Fin Ej 29

--- 29/Program.cs
+++ 29/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿void CantidadEstudiantes()
+{
+    Console.WriteLine("Ingrese la cantidad de estudiantes en el curso");
+    Console.WriteLine();
+    int cantidadestudiantes=int.Parse(Console.ReadLine());
+    Console.WriteLine();
+    if (cantidadestudiantes>=0)
+    {
+        Console.WriteLine($"Hay {cantidadestudiantes} estudiantes en el curso");
+    }
+    else
+    {
+        Console.WriteLine("Cantidad no válida");
+    }
+}
+CantidadEstudiantes();
